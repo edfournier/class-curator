5cae0129b654c4f24450e248acebe9482be0f7ba
unknown <edfournier@umass.edu>
2024-11-23 12:46:16 -0500

    increase ratings count

--- rmp/scrape.py
+++ rmp/scrape.py
@@ -12,28 +12,20 @@ headers = {
     "Referer": "https://www.ratemyprofessors.com/search/professors/1513"
 }
 
-profs_variables = {
-    "count": 1000,  # Page max
-    "cursor": None,
-    "query": {
-        "text": "",
-        "schoolID": "U2Nob29sLTE1MTM=",  # UMass "legacy" ID
-        "fallback": True
-    }
-}
-
-ratings_variables = {
-    "count": 1, 
-    "courseFilter": None, 
-    "cursor": None
-}
-
 def fetch_profs():
     print("Fetching professors...")
+
     profs = []
+    variables = {
+        "count": 1000,  # Page max
+        "query": { 
+            "schoolID": "U2Nob29sLTE1MTM=", # UMass legacy ID
+            "fallback": True }
+    }
+
     while True:
         # Sequential requests because we need to page
-        json = requests.post(url, json={"query": queries.profs, "variables": profs_variables}, headers=headers).json()
+        json = requests.post(url, json={"query": queries.profs, "variables": variables}, headers=headers).json()
         for edge in json["data"]["search"]["teachers"]["edges"]:
             node = edge["node"]
             if node["numRatings"] > 0:  # Filter profs without ratings
@@ -43,38 +35,32 @@ def fetch_profs():
         page_info = json["data"]["search"]["teachers"]["pageInfo"]
         if not page_info["hasNextPage"]:
             break
-        profs_variables["cursor"] = page_info["endCursor"]
+        variables["cursor"] = page_info["endCursor"]
     return profs
+
+async def fetch_prof_ratings(session, prof):
+    # Using upper bound for ratings count
+    variables = {"count": 1000, "id": prof["id"]}
+    res = await session.post(url, json={"query": queries.ratings, "variables": variables}, headers=headers)
+    json = await res.json()
+
+    # Parse response into file format
+    ratings = []
+    for edge in json["data"]["node"]["ratings"]["edges"]:
+        node = edge["node"]
+        ratings.append(f"{node["class"]} {node["difficultyRating"]} {node["helpfulRating"]}\n") 
+    return ratings
 
 async def fetch_ratings(profs):
     print("Fetching ratings...")
-    async with aiohttp.ClientSession() as session:
-        # Async makes this way faster
-        tasks = []
-        for prof in profs:
-            variables = ratings_variables.copy()
-            variables["id"] = prof["id"]
-            tasks.append(session.post(url, json={"query": queries.ratings, "variables": variables}, headers=headers))
-        responses = await asyncio.gather(*tasks)
-        
-        # Parse responses
-        ratings = []
-        for response in responses:
-            json = await response.json()
-            for edge in json["data"]["node"]["ratings"]["edges"]:
-                node = edge["node"]
-                ratings.append(f"{node["class"]} {node["difficultyRating"]} {node["helpfulRating"]}\n") # Formatted for file
-        return ratings
+    # Reuse session for batch requests
+    async with aiohttp.ClientSession() as session: 
+        return await asyncio.gather(*[fetch_prof_ratings(session, prof) for prof in profs])
 
-async def write_ratings():
-    # Fetch ratings for all profs and write to ratings.txt
-    profs = fetch_profs()
-    ratings = await fetch_ratings(profs)
+if __name__ == "__main__":
+    ratings = asyncio.run(fetch_ratings(fetch_profs()))
     with open("ratings.txt", "w", encoding="utf-8") as file:
         file.write("class, difficulty, helpfulness\n")
         for rating in ratings:
             file.writelines(rating)
-
-if __name__ == "__main__":
-    asyncio.run(write_ratings())
     print("Done!")
