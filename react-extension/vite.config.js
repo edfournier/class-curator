d43a58564e03775336caea0b79213a0a553d0236
unknown <edfournier@umass.edu>
2024-11-23 21:16:45 -0500

    remove public folder

--- react-extension/vite.config.js
+++ react-extension/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite"
 import { crx } from "@crxjs/vite-plugin"
 import react from "@vitejs/plugin-react"
-import manifest from "./public/manifest.json"
+import manifest from "./manifest.json"
 
 export default defineConfig({
     plugins: [
