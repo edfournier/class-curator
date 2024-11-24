7b011737e0332f1fc13a2e88365ead191b23eb80
unknown <edfournier@umass.edu>
2024-11-24 15:14:58 -0500

    temp styles

--- react-extension/src/components/Login.jsx
+++ react-extension/src/components/Login.jsx
@@ -3,7 +3,7 @@ import useAuth from "../auth/useAuth.js";
 
 function Login() {
     const auth = useAuth();
-    return auth.user ? <Navigate to={"/home"}/> : (
+    return auth.user && auth.token ? <Navigate to={"/home"}/> : (
         <>
             <button onClick={auth.login}>Login</button>
         </>
