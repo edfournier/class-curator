91bc9293453947bcbf5f89a9644a59aa308d02c0
unknown <edfournier@umass.edu>
2024-11-26 13:15:38 -0500

    highlight active page

--- extension/src/components/Navbar.jsx
+++ extension/src/components/Navbar.jsx
@@ -1,4 +1,18 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
+/**
+ * Creates a stylized link that redirects to the specified path
+ */
+function Link({ name, path }) {
+    return (
+        <NavLink
+            to={path}
+            className={({ isActive }) =>`text-lg transition-colors ${isActive ? 'text-indigo-500 border-b-2 border-indigo-500' : 'hover:text-indigo-500'}`}
+        >
+            {name}
+        </NavLink>
+    );
+}
 
 /**
  * Renders the application's navigation bar, with switching managed by React Router
@@ -8,15 +22,9 @@ function Navbar() {
         <div className="bg-gray-900 text-white py-2 px-4 shadow-md w-full">
             <div className="flex justify-between items-center">
                 <div className="space-x-6">
-                    <Link to="/home" className="text-lg hover:text-indigo-500 transition-colors">
-                        Home
-                    </Link>
-                    <Link to="/friends" className="text-lg hover:text-indigo-500 transition-colors">
-                        Friends
-                    </Link>
-                    <Link to="/courses" className="text-lg hover:text-indigo-500 transition-colors">
-                        Courses
-                    </Link>
+                    <Link name="Home" path="/home"/>
+                    <Link name="Friends" path="/friends"/>
+                    <Link name="Courses" path="/courses"/>
                 </div>
             </div>
         </div>
