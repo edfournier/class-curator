fab59a9c403b9bf040aceef338bb74252b846730
unknown <edfournier@umass.edu>
2024-11-26 12:11:25 -0500

    style login page

--- extension/src/components/Login.jsx
+++ extension/src/components/Login.jsx
@@ -2,11 +2,19 @@ import { Navigate } from "react-router";
 import useAuth from "../auth/useAuth.js";
 
 function Login() {
-    const auth = useAuth();
-    return auth.token ? <Navigate to={"/home"}/> : (
-        <>
-            <button onClick={() => auth.login({ interactive: true })}>Login</button>
-        </>
+    const { token, login } = useAuth();
+
+    if (token) return <Navigate to="/home" />;
+
+    return (
+        <div className="flex items-center justify-center min-h-screen">
+            <button
+                onClick={() => login({ interactive: true })}
+                className="px-6 py-3 bg-indigo-500 text-white text-lg font-semibold rounded-lg hover:bg-indigo-600 active:scale-95"
+            >
+                Login
+            </button>
+        </div>
     );
 }
 
