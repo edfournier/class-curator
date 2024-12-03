c46e4bc567a6aae122641f3d71adf71f036be292
unknown <edfournier@umass.edu>
2024-12-03 16:17:36 -0500

    start friends page

--- extension/src/components/Friends.jsx
+++ extension/src/components/Friends.jsx
@@ -1,13 +1,83 @@
+import { useEffect, useState } from "react";
 import Navbar from "./Navbar";
-
-// TODO: Input box to send a friend request
-// TODO: pagable list of current friends
+import { FaTimes } from "react-icons/fa";
+import { BsSendFill } from "react-icons/bs";
 
 function Friends() {
+    const [friends, setFriends] = useState(null);
+    const [email, setEmail] = useState("");
+    const [page, setPage] = useState(1);
+
+    const maxTiles = 3
+    const last = page * maxTiles;
+    const first = last - maxTiles;
+    const tiles = friends?.slice(first, last)?.map(({ email, name }) => 
+        <div key={email} className="flex justify-between items-center bg-gray-900 text-white p-2 rounded-lg border border-gray-700" >
+            <span>{name} ({email})</span>
+            <FaTimes onClick={() => removeFriend(email)} className="cursor-pointer" />
+        </div>
+    );
+
+    function requestFriend() {
+        // TODO: make API call
+        console.log(email);
+        setEmail("");
+    }
+
+    function removeFriend(target) {
+        // TODO: make API call
+        setFriends(friends.filter((friend) => friend.email !== target));
+    }
+
+    useEffect(() => {
+        // TODO: replace with API call
+        setFriends([
+            { name: "Alice", email: "alice@umass.edu" },
+            { name: "Bob", email: "bob@umass.edu" },
+            { name: "Charlie", email: "charlie@umass.edu" },
+            { name: "David", email: "david@umass.edu" },
+            { name: "Eve", email: "eve@umass.edu" },
+        ]);
+    }, []);
+
     return (
         <>
             <Navbar />
-            <div>Friends</div>
+            <div className="max-w-4xl mx-auto px-2 py-3">
+                <div className="flex justify-center mb-4">
+                    <input
+                        type="text"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        className="w-60"
+                        placeholder="Enter a UMass email to friend..."
+                    />
+                    <button onClick={requestFriend} className="ml-1">
+                        <BsSendFill />
+                    </button>
+                </div>
+
+                <div className="space-y-1">{tiles}</div>
+
+                {friends && (
+                    <div className="flex justify-center mt-2">
+                        <button
+                            disabled={page === 1}
+                            onClick={() => setPage((prev) => prev - 1)}
+                            className="mr-2 disabled:opacity-10"
+                        >
+                            Back
+                        </button>
+                        <button
+                            disabled={last >= friends.length}
+                            onClick={() => setPage((prev) => prev + 1)}
+                            className="disabled:opacity-10"
+                        >
+                            Next
+                        </button>
+                    </div>
+                )}
+            </div>
         </>
     );
 }
--- extension/src/components/Navbar.jsx
+++ extension/src/components/Navbar.jsx
@@ -7,7 +7,12 @@ function Link({ name, path }) {
     return (
         <NavLink
             to={path}
-            className={({ isActive }) =>`text-lg transition-colors ${isActive ? 'text-indigo-500 border-b-2 border-indigo-500' : 'hover:text-indigo-500'}`}
+            className={({ isActive }) =>
+                "text-lg transition-colors pb-2 " + 
+                    isActive
+                        ? "text-indigo-500 border-b-4 border-indigo-500"
+                        : "hover:text-indigo-500 hover:border-b-4 hover:border-indigo-500"
+            }
         >
             {name}
         </NavLink>
@@ -22,9 +27,9 @@ function Navbar() {
         <div className="bg-gray-900 text-white py-2 px-4 shadow-md w-full">
             <div className="flex justify-between items-center">
                 <div className="space-x-6">
-                    <Link name="Home" path="/home"/>
-                    <Link name="Friends" path="/friends"/>
-                    <Link name="Courses" path="/courses"/>
+                    <Link name="Home" path="/home" />
+                    <Link name="Friends" path="/friends" />
+                    <Link name="Courses" path="/courses" />
                 </div>
             </div>
         </div>
