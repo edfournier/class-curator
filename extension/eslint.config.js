20a86900c0c445af8f5522f89edc1d40909c8f9c
edfournier <ericfournier.d@gmail.com>
2024-12-04 10:35:06 -0500

    eslint

--- extension/eslint.config.js
+++ extension/eslint.config.js
@@ -1,5 +1,5 @@
 import js from "@eslint/js";
-import globals from "globals";
+import globals, { browser, webextensions } from "globals";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
@@ -28,11 +28,16 @@ export default [
 			...react.configs.recommended.rules,
 			...react.configs["jsx-runtime"].rules,
 			...reactHooks.configs.recommended.rules,
+            "react/prop-types": "off",
 			"react/jsx-no-target-blank": "off",
 			"react-refresh/only-export-components": [
 				"warn",
 				{ allowConstantExport: true },
 			],
 		},
+        env: {
+            browser: true,
+            webextensions: true
+        }
 	},
 ];
